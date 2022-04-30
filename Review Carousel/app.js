04df87a05ee2d3c82a8f0226feac67e977c518bb
GulsenZalova <zalova0202@gmail.com>
2022-04-30 17:08:35 +0400

    slider made

--- Review Carousel/app.js
+++ Review Carousel/app.js
@@ -2,8 +2,8 @@
 const reviews=[
     {
         id: 1,
-        name:"susan smith",
-        job:"web developer",
+        name:"Susan Smith",
+        job:"WEB DEVELOPER",
         text:"I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
         img:"https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg"
     },
@@ -11,8 +11,8 @@ const reviews=[
 
     {
         id:2,
-        name:"anna johnson",
-        job:"web designer",
+        name:"Anna Johnson",
+        job:"WEV designerDESİGNER",
         text:"Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
         img:"https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg"
 
@@ -20,8 +20,8 @@ const reviews=[
 
     {
         id:3,
-        name:"peter jones",
-        job:"intern",
+        name:"Peter Jones",
+        job:"İNTERN",
         text:"Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
         img:"https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg"
 
@@ -29,8 +29,8 @@ const reviews=[
 
     {
         id:4,
-        name:"bill anderson",
-        job:"the boss",
+        name:"Bill Anderson",
+        job:"THE BOSS",
         text:"Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
         img:"https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg"
 
